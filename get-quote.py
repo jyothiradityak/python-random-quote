7bc3af79161f04c85e4c2c205f34cae5a719175c
jyothiraditya.k <aditya.jyothir@gmail.com>
2021-12-02 23:03:47 +0530

    Close

--- get-quote.py
+++ get-quote.py
@@ -4,15 +4,18 @@ import random
 def primary():
   #print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
+  f = open("quotes.txt","r")
   quotes = f.readlines()
-  f.close()
 
+  #f.writelines(["\nSee you soon!", "\nOver and out."])
+ 
   last = len(quotes) - 1
   rnd = random.randint(0, last)
 
   #print(quotes)
-  print(quotes[rnd])
+  print(quotes[rnd:rnd+2])
+  f.close()
+
 
 
 if __name__== "__main__":
